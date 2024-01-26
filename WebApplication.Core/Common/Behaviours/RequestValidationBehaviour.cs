915b756524baac182c259e48bd2fee9536f84314
meankch <me.kukkig@gmail.com>
2024-01-26 12:33:49 +1100

    add logic to intercept request validation error

--- WebApplication.Core/Common/Behaviours/RequestValidationBehaviour.cs
+++ WebApplication.Core/Common/Behaviours/RequestValidationBehaviour.cs
@@ -1,19 +1,50 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Results;
 using MediatR;
 
 namespace WebApplication.Core.Common.Behaviours
 {
     public class RequestValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
     {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public RequestValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
         /// <inheritdoc />
         public async Task<TResponse> Handle(
             TRequest request,
             CancellationToken cancellationToken,
             RequestHandlerDelegate<TResponse> next)
         {
-            // TODO: throw a validation exception if there are any validation errors
-            // NOTE: the validation exception should contain all failures
+            if (!_validators.Any())
+            {
+                return await next();
+            }
+
+            var context = new ValidationContext<TRequest>(request);
+
+            var errorsDictionary = _validators
+                .Select(req => req.Validate(context))
+                .SelectMany(validationResult => validationResult.Errors)
+                .Where(validationFailure => validationFailure != null)
+                .GroupBy(
+                x => x.PropertyName,
+                x => x.ErrorMessage,
+                (propertyName, errorMessages) =>
+                    new ValidationFailure(propertyName, string.Join(",", errorMessages.Distinct()))
+                );
+
+            if (errorsDictionary.Any())
+            {
+                throw new ValidationException(errorsDictionary);
+            }
 
             return await next();
         }
